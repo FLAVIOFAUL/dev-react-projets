a50ddd648ffddd756cdd52cc2504d80e36608bd3
flaviofaul <flavinhochicshow@gmail.com>
2024-05-27 13:14:29 -0300

    git commit -m "dev-project-reacts

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -6,16 +6,16 @@ export const Button = styled.button`
 
 width: 342px;
 height: 74px;
-border: ${props => props.isBack ? "1px solid #FFFFFF": "none"};
+border: ${props => props.isBack ? "1px solid #ffffff": "none"};
 border-radius: 14px 14px 14px 14px;
-background: ${props => props.isBack ? "transparent": "#000000CC"} ;
+background: ${props => props.isBack ? "transparent": "rgba( 0, 0, 0, 0.8)"} ;
 margin-top: 130px;
 
 font-size: 17px;
 font-weight: bold;
 font-style:normal;
 line-height: 28px;
-color: #FFFFFF;
+color: #ffffff;
 cursor:pointer;
 
 display: flex;
@@ -33,5 +33,7 @@ opacity: 0.5;
 }
 
 img {
-transform: ${props => props.isBack && "rotateY(180deg)"} }
+transform: ${props => props.isBack && "rotateY(180deg)"}
+
+}
 `;
